88fbad1dc90c07d5ec654c9798f40345de40f94f
ramanarolla1-source <ramana.rolla1@gmail.com>
2026-07-11 20:54:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,26 @@
+import Hero from "./components/Hero";
+import ThreeLayers from "./components/ThreeLayers";
+import DecisionJourney from "./components/DecisionJourney";
+import Architecture from "./components/Architecture";
+import TrustReceipt from "./components/TrustReceipt";
+import Footer from "./components/Footer";
 
+export default function Home() {
+  return (
+    <main>
+
+      <Hero />
+
+      <ThreeLayers />
+
+      <DecisionJourney />
+
+      <Architecture />
+
+      <TrustReceipt />
+
+      <Footer />
+
+    </main>
+  );
+}
